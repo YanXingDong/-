5b3e65c4940651a5af3c45ca91f319997a37aa86
YanXingDong <55045825+YanXingDong@users.noreply.github.com>
2019-12-26 17:57:54 +0800

    CourseSelectionSystem

--- CourseSelectionSystem/src/userService/StudentService.java
+++ CourseSelectionSystem/src/userService/StudentService.java
@@ -1,15 +1,36 @@
 package userService;
 
-public class StudentService {
+import database.Student;
+import database.StudentDBUnit;
 
+import java.io.Serializable;
+
+public class StudentService implements Serializable {
+    private static StudentDBUnit studentDBUnit = StudentDBUnit.getInstance();
+    private Student student;
     public String login(String account,String password){
-
-        if(account.equals("闫兴东")&&password.equals("dong13782526217+")){
-            return "登录成功";
+        student = studentDBUnit.getUser(account);
+        if(student!=null){
+            if(account.equals(student.getAccount())&&password.equals(student.getPassword())){
+                return "登录成功";
+            }
+            return "密码或账号错误";
         }
-        return "登录失败";
+        else return "错误，未找到该账号";
     }
     public void register(String account,String password){
+        student = new Student();
+        student.setAccount(account);
+        student.setPassword(password);
+        student.setSex("男");
+        student.setDepartment("软件学院");
+        student.setName("盐行动");
+        try {
+            studentDBUnit.addUser(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println("注册成功！");
     }
 }
